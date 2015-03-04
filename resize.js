edea162096cbb23372ee1864a80f126628f71285
doremi <doremi@Mac>
2015-03-04 14:22:54 +0800

    711 改了網頁結構，因此修正刪除服務單的 selector

--- resize.js
+++ resize.js
@@ -1,7 +1,7 @@
 /*global console, $*/
 
-$('tr:nth(7)').remove();
-$('tr:nth(7)').remove();
+$('tr:nth(8)').remove();
+$('tr:nth(8)').remove();
 $('#imgBarCode4').width("70%");
 $('#imgBarCode1').width("40%");
 $('#imgBarCode2').width("60%");
