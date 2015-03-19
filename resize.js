8c863be41c8dfa8b322312e905451e68ea699727
doremi <doremi@Mac>
2015-03-19 19:08:38 +0800

    提高不必要欄位的透明度 以減低墨水量

--- resize.js
+++ resize.js
@@ -1,10 +1,18 @@
 /*global console, $*/
 
+var OPACITY = 0.3;
+
 $('tr:nth(8)').remove();
 $('tr:nth(8)').remove();
 $('#imgBarCode4').width("70%");
 $('#imgBarCode1').width("40%");
 $('#imgBarCode2').width("60%");
 $('#imgBarCode3').width("60%");
+
+$('.style1').css('opacity', OPACITY);
+$('table[width="98%"]').attr('border', '0');
+$('td[style="width: 309px"]').css('opacity', OPACITY);
+$('.style12').css('opacity', OPACITY);
+$('.thiner_border').css('opacity', OPACITY);
 
 console.log("完成啦！");
