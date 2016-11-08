9967467a1299259eb79c825af81db739f6ed6896
doremi <doremi@Mac>
2016-11-08 13:22:12 +0800

    把每一個條碼距離拉大一點
    version 0.0.6

--- resize.js
+++ resize.js
@@ -6,4 +6,8 @@ var contents = $('#table > tbody > tr');
 contents[3].remove();
 contents[4].remove();
 
+var bars = $('#tableA2 > tbody:first > tr td');
+bars.splice(0, 1);
+bars.css('padding-top', '8px');
+
 console.log("完成啦！");
